abbe454d72b5047c9ab18f4a69b9843f60ee690f
vms <michail.vms@gmail.com>
2020-05-06 00:30:33 +0300

    thanks Mark for help :)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use std::ops::{Deref, DerefMut};
+use std::mem;
+use std::ops::Deref;
 use std::pin::Pin;
 use wasmer_runtime::{compile, Func, ImportObject, Instance};
 
-struct T<'a> {
+struct T {
     instance: Instance,
-    func: Option<Func<'a, i32, i32>>,
+    func: Option<Func<'static, i32, i32>>,
 }
 
-impl T<'_> {
+impl T {
     pub fn new() -> Pin<Box<Self>> {
         let import_object = ImportObject::new();
         let wasm_bytes = vec![];
@@ -15,20 +16,20 @@ impl T<'_> {
             .unwrap()
             .instantiate(&import_object)
             .unwrap();
-
         let mut res = Box::pin(Self {
             instance,
             func: None,
         });
+
         unsafe {
-            //let mut_ref = Pin::as_mut(&mut res);
-            Pin::get_unchecked_mut(res.as_mut()).func = Some(
-                res.deref()
-                    .instance
-                    .exports
-                    .get::<Func<'_, i32, i32>>("asd")
-                    .unwrap(),
-            );
+            let func = res
+                .deref()
+                .instance
+                .exports
+                .get::<Func<'_, i32, i32>>("asd")
+                .unwrap();
+            let static_func = mem::transmute::<Func<'_, i32, i32>, Func<'static, i32, i32>>(func);
+            Pin::get_unchecked_mut(res.as_mut()).func = Some(static_func);
         }
         //res.as_mut().func = func;
 
